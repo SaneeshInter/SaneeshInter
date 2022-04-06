5068eb43e7a481c479788a4528bb055f51169a04
rashidvv <101393154+rashidvv@users.noreply.github.com>
2022-04-06 15:09:19 +0530

    profile screen

--- lib/ui/widgets/profile_detail.dart
+++ lib/ui/widgets/profile_detail.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:xpresshealthdev/UI/user/detail/profile_details_row.dart';
+import 'package:xpresshealthdev/model/user_get_response.dart';
 
 import '../../Constants/app_defaults.dart';
 import '../../utils/constants.dart';
@@ -8,107 +9,124 @@ import '../user/home/profile_edit.dart';
 import 'buttons/drawable_button.dart';
 
 class ProfileDetailCard extends StatelessWidget {
-  const ProfileDetailCard({
-    Key? key,
-  }) : super(key: key);
+
+  Items items;
+
+ProfileDetailCard({Key? key, required this.items}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
+
+    String? firstName = items.firstName;
+    String? lastName =items.lastName;
+    String? employeeNo = items.employeeNo;
+    String? hourlyRate = items.hourlyRate;
+    String? gender = items.gender;
+    String? dob = items.dob;
+    String? homeAddress = items.homeAddress;
+    String? email = items.email;
+    String? phoneNumber =items.phoneNumber;
+    String? ppsNumber = items.ppsNumber;
+    String? bankIban =items.bankIban;
+
+
+
     return Material(
       color: Colors.white,
       borderRadius: AppDefaults.borderRadius,
-      child: InkWell(
-        onTap: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => const ProfileDetailCard()));
-        },
-        borderRadius: AppDefaults.borderRadius,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: AppDefaults.borderRadius,
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.white,
-                  offset: Offset(2, 0),
-                  spreadRadius: 1,
-                )
-              ],
-            ),
-            child: Column(
-              children: [
-                /// TOP PART APPOINTMENT
-                Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: AppDefaults.padding,
-                    vertical: AppDefaults.padding,
-                  ),
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          Text(
-                            'Personal Details',
-                            style: TextStyle(
-                                color: Constants.colors[3],
-                                fontSize: 18,
-                                fontFamily: "SFProMedium",
-                                fontWeight: FontWeight.w700),
-                          ),
-                          const Spacer(),
-                          DrawableButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => ProfileEditScreen()),
-                              );
-                            },
-                            label: "Edit",
-                            asset: "assets/images/icon/swipe-to-right.svg",
-                            backgroundColor: Constants.colors[2],
-                            textColors: Constants.colors[4],
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 8.0),
-                      ProfileDetailsRow(
-                          label: "Gender:Male",
-                          asset: "assets/images/icon/sex.svg"),
-                      SizedBox(width: 15.0),
-                      ProfileDetailsRow(
-                          label: "Date Of Birth:00/00/00",
-                          asset: "assets/images/icon/confetti.svg"),
-                      SizedBox(width: 15.0),
-                      ProfileDetailsRow(
-                          label: "Address",
-                          asset: "assets/images/icon/Pin.svg"),
-                      SizedBox(width: 15.0),
-                      ProfileDetailsRow(
-                          label: "Email",
-                          asset: "assets/images/icon/email.svg"),
-                      SizedBox(width: 15.0),
-                      ProfileDetailsRow(
-                          label: "Phone Number",
-                          asset: "assets/images/icon/phone.svg"),
-                      SizedBox(width: 15.0),
-                      ProfileDetailsRow(
-                          label: "PPS Number",
-                          asset: "assets/images/icon/passport.svg"),
-                      SizedBox(width: 15.0),
-                      ProfileDetailsRow(
-                          label: "Bank Details",
-                          asset: "assets/images/icon/bank.svg"),
-                      SizedBox(width: 15.0),
-                    ],
-                  ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: AppDefaults.borderRadius,
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.white,
+                offset: Offset(2, 0),
+                spreadRadius: 1,
+              )
+            ],
+          ),
+          child: Column(
+            children: [
+              /// TOP PART APPOINTMENT
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: AppDefaults.padding,
+                  vertical: AppDefaults.padding,
                 ),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          'Personal Details',
+                          style: TextStyle(
+                              color: Constants.colors[3],
+                              fontSize: 18,
+                              fontFamily: "SFProMedium",
+                              fontWeight: FontWeight.w700),
+                        ),
+                        const Spacer(),
+                        DrawableButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => ProfileEditScreen()),
+                            );
+                          },
+                          label: "Edit",
+                          asset: "assets/images/icon/swipe-to-right.svg",
+                          backgroundColor: Constants.colors[2],
+                          textColors: Constants.colors[4],
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 8.0),
+                    ProfileDetailsRow(
+                        label: "Gender: "+gender!,
+                        asset: "assets/images/icon/sex.svg"),
 
-                /// Bottom Section
-              ],
-            ),
+
+                    SizedBox(width: 15.0),
+                    ProfileDetailsRow(
+                        label: "Date Of Birth : "+dob!,
+                        asset: "assets/images/icon/confetti.svg"),
+
+
+                    SizedBox(width: 15.0),
+                    if(homeAddress != null)
+                      ProfileDetailsRow(
+                          label: "Address"+ homeAddress,
+                          asset: "assets/images/icon/Pin.svg"),
+
+                    SizedBox(width: 15.0),
+                    if(homeAddress != null)
+
+
+                    ProfileDetailsRow(
+                        label: "Email"+email!,
+                        asset: "assets/images/icon/email.svg"),
+                    SizedBox(width: 15.0),
+                    ProfileDetailsRow(
+                        label: "Phone Number"+phoneNumber!,
+                        asset: "assets/images/icon/phone.svg"),
+                    SizedBox(width: 15.0),
+                    ProfileDetailsRow(
+                        label: "PPS Number"+ppsNumber!,
+                        asset: "assets/images/icon/passport.svg"),
+                    SizedBox(width: 15.0),
+                    ProfileDetailsRow(
+                        label: "Bank Details"+bankIban!,
+                        asset: "assets/images/icon/bank.svg"),
+                    SizedBox(width: 15.0),
+                  ],
+                ),
+              ),
+
+              /// Bottom Section
+            ],
           ),
         ),
       ),
